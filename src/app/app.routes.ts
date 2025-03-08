16a5b2c77840eb61f3a39b8d5c99474e3aeb1728
Camila Amaro <camilaamarov@yahoo.es>
2025-03-07 19:39:47 -0500

    feat: Add project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CaseStudyComponent } from './pages/case-study/case-study.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/case-study', pathMatch: 'full'},
+    {path: 'case-study', component: CaseStudyComponent }
+];
