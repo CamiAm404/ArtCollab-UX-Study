67a00213b46f5f1ddaf446b3780dfe582dc945d5
Camila Amaro <camilaamarov@yahoo.es>
2025-03-07 23:40:00 -0500

    feat: Add README.md

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { CaseStudyComponent } from './pages/case-study/case-study.component';
-
-export const routes: Routes = [
-    {path: '', redirectTo: '/case-study', pathMatch: 'full'},
-    {path: 'case-study', component: CaseStudyComponent }
-];
